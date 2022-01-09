48eaf94df65d0f235455467f67208cad600f3909
Lorenzo <smirnov_bvb@mail.ru>
2022-01-09 22:18:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2_000;
+        int amount = 20;
+        int miles = price / amount;
+
+        System.out.println(miles);
+    }
+}
